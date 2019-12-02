c6fd1d28e321eb5dda53b56aabba3252fbee3320
Simon Walker <github@stwalkerster.co.uk>
2019-12-02 23:54:09 +0000

    more infrastructure surrounding zone edting

--- DnsWebApp/Controllers/ZoneController.cs
+++ DnsWebApp/Controllers/ZoneController.cs
@@ -34,13 +34,34 @@ namespace DnsWebApp.Controllers
         [Route("/zone/{zone:int}")]
         public IActionResult ShowZone(int zone)
         {
-            return Content("zonefile for " + zone);
+            return this.View(this.db.Zones
+                .Include(x => x.TopLevelDomain)
+                .Include(x => x.Owner)
+                .Include(x => x.Registrar)
+                .FirstOrDefault(x => x.Id == zone));
         }
 
         [Route("/zone/new")]
         public IActionResult NewZone()
         {
-            return Content("Creating new zone ");
+            return this.View(new Zone());
+        }
+        
+        [Route("/zone/edit/{zone:int}")]
+        public IActionResult EditZone(int zone)
+        {
+            return this.View(this.db.Zones
+                .Include(x => x.TopLevelDomain)
+                .Include(x => x.Owner)
+                .Include(x => x.Registrar)
+                .FirstOrDefault(x => x.Id == zone));
+        }
+
+        [HttpPost]
+        [Route("/zone/togglefave")]
+        public IActionResult ToggleFave(int zone, string returnto)
+        {
+            return this.Redirect(returnto);
         }
     }
 }
