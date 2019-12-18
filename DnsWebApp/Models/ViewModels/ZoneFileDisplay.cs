5850e203ea4f0efc54ef2109ed438c16ea6c5229
Simon Walker <github@stwalkerster.co.uk>
2019-12-18 00:39:58 +0000

    Fix zonefile display for empty zone

--- DnsWebApp/Models/ViewModels/ZoneFileDisplay.cs
+++ DnsWebApp/Models/ViewModels/ZoneFileDisplay.cs
@@ -1,6 +1,8 @@
 namespace DnsWebApp.Models.ViewModels
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using DnsWebApp.Models.Database;
 
     public class ZoneFileDisplay
@@ -12,7 +14,33 @@ namespace DnsWebApp.Models.ViewModels
         
         public Zone Zone { get; set; }
         public string Fqdn { get; set; }
-        
         public List<Record> Records { get; }
+
+        public int NameMinWidth
+        {
+            get
+            {
+                var recordMax = 0;
+                if (this.Records.Any())
+                {
+                    recordMax=this.Records.Max(x => (x.Name?.Length).GetValueOrDefault(1) + 1);
+                }
+
+                return Math.Max(8, recordMax);
+            }
+        }
+        public int TtlMinWidth
+        {
+            get
+            {
+                var recordMax = 0;
+                if (this.Records.Any())
+                {
+                    recordMax = this.Records.Max(x => x.TimeToLive.ToString().Length + 1);
+                }
+
+                return Math.Max(0, recordMax);
+            }
+        }
     }
 }
